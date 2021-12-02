448c29a62757c0b48315828cc020bcd7c9b3eee7
Phil Davis <phil@jankaritech.com>
2021-12-02 17:35:49 +0545

    Merge smoke-test login steps

--- tests/smoke/features/step_definitions/session.ts
+++ tests/smoke/features/step_definitions/session.ts
@@ -1,34 +1,24 @@
-import { Given, When } from '@cucumber/cucumber'
+import { Given } from '@cucumber/cucumber'
 import { World, config, LoginPage, RuntimePage } from '../../support'
 
-Given('{string} has logged in', async function (this: World, stepUser: string): Promise<void> {
-  const user = this.userContinent.get({ id: stepUser })
-  const actor = await this.actorContinent.create({ id: stepUser })
-  const loginPage = new LoginPage({ actor })
+Given(
+  '{string} (has logged|logs) in',
+  async function (this: World, stepUser: string, _: string): Promise<void> {
+    const user = this.userContinent.get({ id: stepUser })
+    const actor = await this.actorContinent.create({ id: stepUser })
+    const loginPage = new LoginPage({ actor })
 
-  await actor.page.goto(config.frontendUrl)
-  await loginPage.login({ user })
-})
+    await actor.page.goto(config.frontendUrl)
+    await loginPage.login({ user })
+  }
+)
 
-When('{string} logs in', async function (this: World, stepUser: string): Promise<void> {
-  const user = this.userContinent.get({ id: stepUser })
-  const actor = await this.actorContinent.create({ id: stepUser })
-  const loginPage = new LoginPage({ actor })
-
-  await actor.page.goto(config.frontendUrl)
-  await loginPage.login({ user })
-})
-
-Given('{string} has logged out', async function (this: World, stepUser: string): Promise<void> {
-  const actor = await this.actorContinent.get({ id: stepUser })
-  const runtimePage = new RuntimePage({ actor })
-  await runtimePage.logout()
-  await actor.close()
-})
-
-When('{string} logs out', async function (this: World, stepUser: string): Promise<void> {
-  const actor = await this.actorContinent.get({ id: stepUser })
-  const runtimePage = new RuntimePage({ actor })
-  await runtimePage.logout()
-  await actor.close()
-})
+Given(
+  '{string} (has logged|logs) out',
+  async function (this: World, stepUser: string, _: string): Promise<void> {
+    const actor = await this.actorContinent.get({ id: stepUser })
+    const runtimePage = new RuntimePage({ actor })
+    await runtimePage.logout()
+    await actor.close()
+  }
+)
